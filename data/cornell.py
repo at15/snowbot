7656fd2076a054fdde8dcb806b70fc355e1184bc
at15 <at15@outlook.com>
2017-12-05 23:43:47 -0800

    [data] cornell_data -> cornell
    - use `from .cornell import XXX` to import, instead of using `import cornell`
      - so no longer need to mark it as source root

--- data/cornell.py
+++ data/cornell.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-# NOTE: note it is named as cornell_data because cornell is already used for the extracted data folder
-
 NAME = 'cornell'
 FULL_NAME = 'Cornell Movie Dialog Corpus'
 URL = 'https://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html'
 DATA_URL = 'http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip'
+
 
 
 def main():
--- data/downloader.py
+++ data/downloader.py
@@ -4,22 +4,22 @@ import os
 import shutil
 import click
 
-import util
-import cornell_data as cornell
+from .util import maybe_extract, maybe_download
+from .cornell import NAME as C_NAME, DATA_URL as C_DURL
 
 
 @click.command()
 @click.argument('dataset')
 def cli(dataset):
     dataset = dataset.lower()
-    if dataset == cornell.NAME:
+    if dataset == C_NAME:
         # TODO: maybe add a force remove folder
         dst = 'raw/cornell'
         if os.path.exists(dst):
             print('extracted cornell data already exist')
             return
-        file = util.maybe_download(cornell.DATA_URL, download_dir='raw')
-        util.maybe_extract(file, 'cornell-tmp')
+        file = maybe_download(C_DURL, download_dir='raw')
+        maybe_extract(file, 'cornell-tmp')
         shutil.move('cornell-tmp/cornell movie-dialogs corpus', dst)
         shutil.rmtree('cornell-tmp', ignore_errors=True)
     else:
